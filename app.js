8caf08fdd9b31c083576cf3e53d0db0fda2141e4
Nasser <nasser02@yahoo.com>
2021-03-23 18:34:35 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Nasser learning CI with AWS'));
+app.get('/', (req, res) => res.send('Hello World! This is Nasser learning CI with AWS'.  I adding changes to this message!!));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
